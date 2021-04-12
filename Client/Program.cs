7a40505237948353b8f6b5bf80d0c36648729bb3
weird-frog <den.6667531@gmail.com>
2021-04-12 23:05:06 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Communicate("localhost", 8888);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadKey();
+            }
+        }
+
+        private static void Communicate(string hostname, int port)
+        {
+            byte[] bytes = new byte[1024];
+
+            IPHostEntry ipHost = Dns.GetHostEntry(hostname);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sock = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(ipEndPoint);
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+            Console.WriteLine($"Подключаемся к порту {sock.RemoteEndPoint.ToString()}");
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            int bytesSent = sock.Send(data);
+            int bytesRec = sock.Receive(bytes);
+
+            Console.WriteLine($"\nОтвет от сервера: {Encoding.UTF8.GetString(bytes, 0, bytesRec)} \n\n");
+
+            if (message.IndexOf("<TheEnd>") == -1)
+            {
+                Communicate(hostname, port);
+            }
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
+        }
+    }
+}
---
+++ PR19-20/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR19_20
+{
+    //серверная часть приложения
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Однопоточный сервер запущен");
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 8888);
+
+            Socket sock = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sock.Bind(ipEndPoint);
+                sock.Listen(10);
+                while (true)
+                {
+                    Console.WriteLine($"Слушаем порт {ipEndPoint}");
+                    Socket s = sock.Accept();
+                    string data = null;
+
+                    byte[] bytes = new byte[1024];
+                    int bytesCount = s.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesCount);
+                    Console.WriteLine("Данные от клиента: " + data + "\n\n");
+
+                    string reply = "Query size: " + data.Length.ToString() + " chars";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    s.Send(msg);
+
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("Соединение завершено.");
+                        break;
+                    }
+                    s.Shutdown(SocketShutdown.Both);
+                    s.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
